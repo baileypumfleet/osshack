8a651dc582c283ecfd529e80d8651f7d4e74b454
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-11-15 17:05:35 +0000

    Auto-create from GitHub

--- app/routes/dashboard.new.tsx
+++ app/routes/dashboard.new.tsx
@@ -7,6 +7,7 @@ import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import Footer from "../components/Footer";
 import { getAuth } from "@clerk/remix/ssr.server";
 import { createClerkClient } from "@clerk/remix/api.server";
+import { Octokit } from "octokit";
 
 export const meta: MetaFunction = () => {
   return [
@@ -35,16 +36,17 @@ export const action = async (args) => {
   const title = formData.get("title");
   const description = formData.get("description");
   const value = formData.get("reward");
+  const github = formData.get("github");
 
   // Update the user with the Prisma client
   await prisma.bounty.create({
     data: {
       title,
       description,
+      github,
       value: parseInt(value as string),
       project: { connect: { id: user?.projectId || 1 } },
     },
-    
   });
 
   return redirect("/dashboard");
@@ -53,6 +55,8 @@ export const action = async (args) => {
 export default function New() {
   const actionData = useActionData();
   const data = useLoaderData<typeof loader>();
+  
+  console.log(data.issues);
 
   return (
     <div>
@@ -61,7 +65,36 @@ export default function New() {
           <h2 className="text-3xl font-cal text-orange-900 sm:text-5xl my-8">
             Automatically create from GitHub
           </h2>
-          <h2 className="text-3xl font-cal text-orange-900 sm:text-5xl my-8">
+          <div className="bg-white rounded-md text-gray-900 text-sm shadow-sm py-1">
+            {data.issues?.data?.map((issue) => (
+              <div key={issue.id} className="px-4 py-2 border-b flex">
+                <div className="font-medium pt-1.5">{issue.title}</div>
+                <div className="ml-auto">
+                  <Form method="post">
+                    <input type="hidden" name="title" value={issue.title} />
+                    <input type="hidden" name="description" value={issue.body || "Unknown"} />
+                    <input type="hidden" name="github" value={issue.html_url} />
+                    <input
+                      type="number"
+                      name="reward"
+                      placeholder="$20"
+                      className="rounded-md w-16 px-2 py-1 outline-none text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                    />
+                    <button className="rounded-md bg-orange-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline">
+                      Create bounty
+                    </button>
+                  </Form>
+                </div>
+              </div>
+            ))}
+            <div className="px-4 pt-4 pb-3 flex text-gray-400">
+              <span className="font-medium text-gray-500">
+                Can&apos;t find what you&apos;re looking for?
+              </span>
+              &nbsp;Reach out to support, or create the issue manually.
+            </div>
+          </div>
+          <h2 className="text-3xl font-cal text-orange-900 sm:text-5xl mb-8 mt-16">
             Manually create a bounty for {data.user?.project?.name || "Unknown"}
           </h2>
           <Form method="post" className="space-y-4">
@@ -83,6 +116,17 @@ export default function New() {
               <textarea
                 name="description"
                 placeholder="Add a detailed description"
+                className="block w-full rounded-md border-0 px-4 py-1.5 outline-none text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                GitHub Link
+              </label>
+              <input
+                type="text"
+                name="github"
+                placeholder="https://github.com/calcom/cal.com/pull/12374"
                 className="block w-full rounded-md border-0 px-4 py-1.5 outline-none text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
               />
             </div>
@@ -125,6 +169,9 @@ export const loader = async (args) => {
   const clerkUser = await createClerkClient({
     secretKey: process.env.CLERK_SECRET_KEY,
   }).users.getUser(userId || "");
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
 
   // Lookup the user with the Prisma client
   const user = await prisma.user.findUnique({
@@ -134,5 +181,10 @@ export const loader = async (args) => {
     },
   });
 
-  return json({ user });
+  const issues = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: "calcom",
+    repo: "cal.com",
+  });
+
+  return json({ user, issues });
 };
