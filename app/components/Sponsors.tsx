6fee0b795716b28fb0f43569a4fd928cf9477031
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-11-21 23:03:10 +0000

    Switch to Cal.com SVG logo

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -8,7 +8,7 @@ export default function Sponsors() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           <img
             className="col-span-2 max-h-7 w-full object-contain lg:col-span-1 -mt-2"
-            src="/logos/calcom.png"
+            src="/logos/cal.svg"
             alt="Cal.com"
           />
           <img
