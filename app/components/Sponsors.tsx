32b9138bfe137a2fca098ee714eff1d61e04caf4
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-11-21 21:54:04 +0000

    Update sponsors on website

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -5,39 +5,61 @@ export default function Sponsors() {
         <h2 className="mt-2 text-3xl font-cal text-orange-900 sm:text-5xl">
           Sponsors
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           <img
-            className="col-span-2 max-h-6 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-7 w-full object-contain lg:col-span-1 -mt-2"
             src="/logos/calcom.png"
             alt="Cal.com"
           />
           <img
-            className="col-span-2 max-h-6 w-full object-contain lg:col-span-1"
-            src="/logos/documenso.png"
-            alt="Documenso"
-            width={158}
-            height={48}
+              className="col-span-2 col-start-2 max-h-10 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="/logos/google.png"
+              alt="Google"
           />
           <img
-            className="col-span-2 max-h-8 w-full object-contain lg:col-span-1"
-            src="/logos/cornell.png"
-            alt="Cornell Tech"
-            width={158}
-            height={48}
+              className="col-span-2 col-start-2 max-h-32 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="/logos/redbull.png"
+              alt="Red Bull"
           />
           <img
-            className="col-span-2 max-h-9 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="/logos/formbricks.png"
-            alt="Formbricks"
-            width={158}
-            height={48}
+              className="col-span-2 max-h-8 w-full object-contain lg:col-span-1"
+              src="/logos/cornell.png"
+              alt="Cornell Tech"
           />
           <img
-            className="col-span-2 col-start-2 max-h-8 w-full object-contain sm:col-start-auto lg:col-span-1"
+              className="col-span-2 max-h-8 w-full object-contain lg:col-span-1"
+              src="/logos/andrew.png"
+              alt="Andrew Yeung"
+          />
+          <img
+              className="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
+              src="/logos/numi.png"
+              alt="Numi"
+          />
+          <img
+              className="col-span-2 max-h-6 w-full object-contain lg:col-span-1"
+              src="/logos/onesafe.png"
+              alt="Onesafe"
+          />
+          <img
+              className="col-span-2 max-h-6 w-full object-contain lg:col-span-1"
+              src="/logos/remotebase.png"
+              alt="Remotebase"
+          />
+          <img
+              className="col-span-2 max-h-6 w-full object-contain lg:col-span-1"
+              src="/logos/documenso.png"
+              alt="Documenso"
+          />
+          <img
+              className="col-span-2 max-h-9 w-full object-contain sm:col-start-2 lg:col-span-1"
+              src="/logos/formbricks.png"
+              alt="Formbricks"
+          />
+          <img
+            className="col-span-2 col-start-2 max-h-10 w-full object-contain sm:col-start-auto lg:col-span-1"
             src="/logos/jacobs.png"
             alt="Jacobs"
-            width={158}
-            height={48}
           />
         </div>
       </div>
