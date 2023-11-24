73dc48eb4ebecd736ee034dd2455c78a8884ea84
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-11-24 14:53:35 +0000

    View on GitHub improvement

--- app/routes/bounty.$id.tsx
+++ app/routes/bounty.$id.tsx
@@ -10,6 +10,7 @@ import remarkGithub from "remark-github";
 import removeComments from "remark-remove-comments";
 import Footer from "~/components/Footer";
 import {
+    ChevronRightIcon,
     CloudArrowDownIcon,
     CogIcon,
     ExclamationTriangleIcon,
@@ -184,41 +185,58 @@ export default function Bounty() {
                         <div>
                             <h2 className="text-3xl font-cal text-orange-900 mb-2">Reward</h2>
                             <p className="text-gray-800 mb-8">
-                <span className="font-semibold text-gray-900">
-                  ${data.bounty?.value}
-                </span>{" "}
+                                <span className="font-semibold text-gray-900">
+                                  ${data.bounty?.value}
+                                </span>{" "}
                                 paid after approval of your submission, redeemable via Tremendous.
                             </p>
 
                             <h2 className="text-3xl font-cal text-orange-900 mb-2">GitHub</h2>
-                            <p className="text-gray-900">
-                                {data.bounty?.github ||
-                                    "We can't seem to find the GitHub link. Contact the team for help."}
-                            </p>
+                            <a href={data.bounty?.github || ""} target="_blank" rel="noreferrer" className="flex space-x-4 text-gray-900 border bg-white rounded-lg px-5 py-2">
+                                <div className="pt-1.5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-gray-900"
+                                         viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                                    </svg>
+                                </div>
+                                <div className="pt-0.5 mb-1.5 w-full flex">
+                                    <div>
+                                        <span className="text-sm text-gray-500">View on</span>
+                                        <p className="text-gray-900 font-medium text-xl leading-5">GitHub</p>
+                                    </div>
+                                    <span className="ml-auto mt-3 text-gray-500">
+                                        <ChevronRightIcon className="h-6 w-6"/>
+                                    </span>
+                                </div>
+                            </a>
 
                             {data.user?.projectId === data.bounty?.project.id && (
-                                <div className="flex space-x-4 mt-4">
-                                    <Form method="patch">
-                                        <input type="hidden" name="bounty" value={params.id}/>
-                                        <button
-                                            type="submit"
-                                            className="text-blue-600 bg-blue-100 px-2 py-1 text-sm rounded-lg flex font-medium"
-                                        >
-                                            <CloudArrowDownIcon className="h-4 w-4 mr-1 mt-0.5"/>
-                                            Update description
-                                        </button>
-                                    </Form>
-                                    <Form method="delete">
-                                        <input type="hidden" name="bounty" value={params.id}/>
-                                        <button
-                                            type="submit"
-                                            className="text-red-600 bg-red-100 px-2 py-1 text-sm rounded-lg flex font-medium"
-                                        >
-                                            <TrashIcon className="h-4 w-4 mr-1 mt-0.5"/>
-                                            Delete bounty
-                                        </button>
-                                    </Form>
-                                </div>
+                                <>
+                                    <h2 className="text-3xl font-cal text-orange-900 mt-8 mb-2">Admin</h2>
+                                    <div className="flex space-x-4 mt-4">
+                                        <Form method="patch">
+                                            <input type="hidden" name="bounty" value={params.id}/>
+                                            <button
+                                                type="submit"
+                                                className="text-blue-600 bg-blue-100 px-2 py-1 text-sm rounded-lg flex font-medium"
+                                            >
+                                                <CloudArrowDownIcon className="h-4 w-4 mr-1 mt-0.5"/>
+                                                Update description
+                                            </button>
+                                        </Form>
+                                        <Form method="delete">
+                                            <input type="hidden" name="bounty" value={params.id}/>
+                                            <button
+                                                type="submit"
+                                                className="text-red-600 bg-red-100 px-2 py-1 text-sm rounded-lg flex font-medium"
+                                            >
+                                                <TrashIcon className="h-4 w-4 mr-1 mt-0.5"/>
+                                                Delete bounty
+                                            </button>
+                                        </Form>
+                                    </div>
+                                </>
                             )}
                         </div>
                     </div>
